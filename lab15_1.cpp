fcf57314ae90bd89e91757faeb8f5981e462b5e3
Naywasd <sermsub2549@gmail.com>
2026-02-01 23:30:21 +0700

    Update lab15_1.cpp

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,35 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    int a = 5;
+    char b = 'A';
 
-	
-	
-	return 0;
+    char &c = b;
+    int *x = &a;
+    
+    char *y = &b;
+    int **z = &x;
+    
+    auto printValues = [&]() {
+        cout << a << " " << b << " " << c << " " << x << " " << (void*)y << " " << z << endl;
+    };
+
+    printValues();
+
+    cout << &a << " " << (void*)&b << " " << (void*)&c << " " << &x << " " << &y << " " << &z << endl;
+
+    c = 'F';
+    printValues();
+
+    *y = 'W';
+    printValues();
+
+    *x = 6;
+    printValues();
+
+    **z = 7;
+    printValues();
+
+    return 0;
 }
